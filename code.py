b9a11d768212cf0d3bd2596f6d321cb694c1bcaa
Aiden <33640860+SomeAspy@users.noreply.github.com>
2021-02-24 12:16:55 -0500

    why do I torture myself like this

---
+++ code.py
@@ -0,0 +1,9 @@
+# Aiden B.
+# Mad Libs Thing
+# 2/24/2021
+
+print("Michael Bay\nThis is a madlibs story I shamelessly ripped off the internet.\nYou play by entering whatever is asked for. its hard to mess up")
+
+# Magic stuff owo
+
+print("Man's Name"+" is a normal "+"Occupation"+". Then, one day, a\n"+"noun"+" explodes, causing a "+"noun"+" to blow up, and a nearby\n"+"noun"+" erupts into a "+"shape"+" of flames. "+"Man's name"+" realizes that\nhe's being chased ny the government, who's trying to "+"verb"+" him. While\non the run, he teams up with an incredibly attractive woman named\n"+"Woman's Name"+", who has an incredible "+"body part"+". She may be\nfrom the streets, but she can "+"Verb"+" like nobody's business. the duo\ndecide to turn the tables on their pursuers by blowing up a "+"noun"+", which\ntriggers a chain reaction, causing the local "+"noun"+", "+"restaurants name"+", and\nthe"+"historic monument"+" to explode. Then, the bad guys' helicopter gets\n"+"verb"+" by a piece of "+"noun"+"from when the "+"noun"+" exploded,\nand the helicopter explodes and falls into a "+"noun"+", causing it to "+"Verb"+",\nwhich shoots a fireball straight into the heart of "+"noun"+" and destroys the\nbad guy leader. everything is "+"Adjective"+" and the two decide that such a\n"+"adjective"+" ordeal; has caused them to fall in "+"emotion"+" with each other.\nThey descide to celebrate by "+"verb"+"ing on the "+"noun"+", and they even\nmanaged to use a "+"noun"+" from the beginning to the movie to "+"verb"+"\nthe whole story together.")
